448bf42e1253e7676454d445eb76e84f6528beb1
jey1601 <fernandoespinalguevara@gmail.com>
2025-04-03 16:54:59 -0600

    División de repositorios

--- storage.tf
+++ storage.tf
@@ -9,9 +9,15 @@ resource "azurerm_storage_account" "samedia" {
   
 }
 
+resource "azurerm_storage_container" "container" {
+  name                  = "media"
+  storage_account_id    = azurerm_storage_account.samedia.id
+  container_access_type = "private"
 
-resource "azurerm_storage_account" "samessages" {
-    name =  "${lower(var.project_name)}${lower(var.repository_name)}samessages"
+}
+
+resource "azurerm_storage_account" "saqueue" {
+    name =  "${lower(var.project_name)}${lower(var.repository_name)}saqueue"
     resource_group_name = azurerm_resource_group.rg.name
     location = var.location
     account_tier = "Standard"
@@ -21,6 +27,10 @@ resource "azurerm_storage_account" "samessages" {
   
 }
 
+resource "azurerm_storage_queue" "queue" {
+  name                 = "procesosasincronos"
+  storage_account_name = azurerm_storage_account.saqueue.name
+}
 
 
 
